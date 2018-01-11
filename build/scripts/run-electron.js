d7b4966b454e9b9dfd48edfd89ab31cc50fa0bb5
alexander <apache3dk@gmail.com>
2018-01-12 00:56:14 +0300

    start-app fix

--- build/scripts/run-electron.js
+++ build/scripts/run-electron.js
@@ -1,8 +1,5 @@
 import opn from 'opn'
-import path from 'path'
 import { basePath } from '../../project.config'
 
-const electron = path.resolve(basePath, 'node_modules/.bin/electron')
-
-opn(basePath, { app: electron, wait: false })
-process.exit()
+opn('', { app: ['electron', basePath], wait: false })
+process.exit(0)
